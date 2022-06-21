d5cbb8a6ee19edb8363842a32595c43856373c56
Sergey <Serduyksb@gimai.com>
2022-06-21 14:08:18 +0300

    Initial commit

---
+++ src/main/java/ru/serdyuk/MyChatBot.java
@@ -0,0 +1,64 @@
+package ru.serdyuk;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyledDocument;
+
+/**
+ * @author Serdyuk S.B.
+ */
+public class MyChatBot extends JFrame implements ActionListener {
+    final String TITLE_OF_PROGRAM = "Chatter: simple chatbot";
+    final int START_LOCATION = 200;
+    final int WINDOW_WIDTH = 350;
+    final int WINDOW_HEIGHT = 450;
+
+    JTextArea dialogue;
+    JCheckBox ai;
+    JTextField message;
+    SimpleBot simpleBot;
+
+    public static void main(String[] args) {
+        new MyChatBot();
+    }
+
+    MyChatBot() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+        //area for dialog
+        dialogue = new JTextArea();
+        dialogue.setLineWrap(true);
+        JScrollPane scrollBar = new JScrollPane(dialogue);
+        // panel for checkbox, message field and button
+        JPanel bp = new JPanel();
+        bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
+        ai = new JCheckBox("AI");
+        //ai.doClick();
+        message = new JTextField();
+        message.addActionListener(this);
+        JButton enter = new JButton("Enter");
+        enter.addActionListener(this);
+        // adding all elements to the window
+        bp.add(ai);
+        bp.add(message);
+        bp.add(enter);
+        add(BorderLayout.CENTER, scrollBar);
+        add(BorderLayout.SOUTH, bp);
+        setVisible(true);
+        simpleBot = new SimpleBot();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (message.getText().trim().length() > 0) {
+            dialogue.append(message.getText().concat("\n"));
+            dialogue.append(TITLE_OF_PROGRAM.substring(0,9) +
+                    simpleBot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+        }
+        message.setText("");
+        message.requestFocusInWindow();
+    }
+}
